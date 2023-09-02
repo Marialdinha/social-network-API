1b769f208f316a4a6560422115be3f8b5ff0d16b
Marialdinha <marialda@bellsouth.net>
2023-09-02 13:29:45 -0500

    Adding routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,9 @@
+// Endpoint = '/api/thoughts'
+const router = require('express').Router();
+
+const {
+    // getUsers,
+    // getSingleUser,
+    // createUser,
+  } = require('../../controllers/thought-controller');
+
---
+++ routes/api/user-routes.js
@@ -0,0 +1,8 @@
+// Endpoint = '/api/users'
+const router = require('express').Router();
+
+const {
+    // getUsers,
+    // getSingleUser,
+    // createUser,
+  } = require('../../controllers/user-controller');
